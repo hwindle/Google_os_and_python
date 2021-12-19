1717f0329d46c216bba1c962f215ff609bfaf269
Hazel Windle <hwindle83@googlemail.com>
2021-12-19 13:22:42 +0000

    Finished and tested info_processing function

--- final_project/ticky_check.py
+++ final_project/ticky_check.py
@@ -22,19 +22,22 @@ def info_processing(file_contents):
     """
     per_user = {}
     info_pattern = r'ticky: INFO'
-
     for line in file_contents:
         # search for info or error
-        user = re.findall(r'\([a-zA-Z ]*\)$', line.strip())
+        user_list = re.findall(r'\([a-zA-Z ]*\)$', line.strip())
+        # append user key to dict.
+        for u in user_list:
+            user_clean = u[1:-1] # take () off
+        if user_clean not in per_user:
+            per_user[user_clean] = {'INFO': 0, 'ERROR': 0} # initialise inner dict.
+        # increment info and error
         if re.search(info_pattern, line) != None:
             # Info line
-            print('info')
-            print(user)
+            per_user[user_clean]['INFO'] = per_user[user_clean]['INFO'] + 1
         else:
-            print('error')
-            #per_user[user] =+ 1
-            print(user)
-    return {}
+            per_user[user_clean]['ERROR'] = per_user[user_clean]['ERROR'] + 1
+    # sort the dictionary by user a-z, returns list of tuples for each dict row.
+    return sorted(per_user.items(), key = operator.itemgetter(0))
 
 
 def error_processing(file_contents):
@@ -48,6 +51,7 @@ def error_processing(file_contents):
         if re.search(error_pattern, line) != None:
             error_text = re.findall(error_pattern, line.strip())
             print(error_text)
+    return error
 
 def user_csv_generator(per_user):
     pass
@@ -59,10 +63,12 @@ def error_csv_generator(error):
 if __name__ == '__main__':
     if sys.argv[1] != None:
         file_contents = get_data()
-        per_user = info_processing(file_contents)
+        #per_user = info_processing(file_contents)
+        error = error_processing(file_contents)
+        print(error)
     try:
         pass
-            #error = error_processing(file_contents)
+            #
             #user_csv_generator(per_user)
             #error_csv_generator(error)
 
