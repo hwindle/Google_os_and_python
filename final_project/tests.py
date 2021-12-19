2d2f0487d84351a89b4304cef0ef5f1d2c767178
Hazel Windle <hwindle83@googlemail.com>
2021-12-19 10:29:11 +0000

    Modified unit tests and the functions.

--- final_project/tests.py
+++ final_project/tests.py
@@ -1,13 +1,34 @@
 import unittest
+import ticky_check
 
+class Ticky(unittest.TestCase):
 
-class TestSum(unittest.TestCase):
+    def test_info_dict(self):
+        info_sample = {
+            'User': 'ac', 'INFO': 2, 'ERROR': 2,
+            'User': 'ahmed.miller', 'INFO': 2, 'ERROR': 4,
+            'User': 'blossom', 'INFO': 2, 'ERROR': 6,
+            'User': 'bpacheco', 'INFO': 0, 'ERROR': 2,
+            'User': 'breee', 'INFO': 1, 'ERROR': 5,
+            'User': 'britanni', 'INFO': 1, 'ERROR': 1,
+            'User': 'enim.non', 'INFO': 2, 'ERROR': 3,
+            'User': 'flavia', 'INFO': 0, 'ERROR': 5,
+            'User': 'ac', 'INFO': 2, 'ERROR': 2,
+        }
+        info = info_processing(file_contents)
+        self.assertEqual(info, info_sample)
 
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+    def test_error_dict(self):
+        error_sample = {
+            'Error': 'Timeout while retrieving information', 'Count': 15,
+            'Error': 'Connection to DB failed', 'Count': 13,
+            'Error': 'Tried to add information to closed ticket', 'Count': 12,
+            'Error': 'Permission denied while closing ticket', 'Count': 10,
+            'Error': 'The ticket was modified while updating', 'Count': 9,
+            'Error': 'Ticket doesn\'t exist', 'Count': 7
+        }
+        errors = error_processing(file_contents)
+        self.assertEqual(errors, error_sample)
 
 if __name__ == '__main__':
     unittest.main()
--- final_project/ticky_check.py
+++ final_project/ticky_check.py
@@ -3,33 +3,46 @@
 import sys, operator
 import re, csv
 
-def create_dictionaries():
-    """
-        File IO for sys log file.
-        Outputs info dict of users,
-        errors dict and freq of errors. Unsorted.
-    """
-    per_user = {}
-    error = {}
-    info_pattern = r'ticky: INFO'
-    error_pattern = r'ERROR ([\w ]*) \('
 
+def get_data():
+    # gets the data from the 1st argument passed in. Returns all lines.
     try:
         with open(sys.argv[1], 'r') as f_open:
             lines = f_open.readlines()
-            for line in lines:
-                # search for info or error
-                if re.search(info_pattern, line) != None:
-                    # Info line
-                    user = re.find(r'\([\w*]\)$', line.strip())
-                    #per_user[user] =+ 1
-                    print(user)
-                # if info or error, log username, info count, error count
-                # if only error, log error msg and count to errors dict.
+            return lines
     except:
         print('File {} cannot be opened', sys.argv[1])
         sys.exit(1)
 
+
+def info_processing(file_contents):
+    """
+        Input: all lines from the log file.
+        Outputs info dict of users in a sorted fashion.
+    """
+    per_user = {}
+    info_pattern = r'ticky: INFO'
+
+    for line in file_contents:
+        # search for info or error
+        if re.search(info_pattern, line) != None:
+            # Info line
+            user = re.find(r'\([\w*]\)$', line.strip())
+            #per_user[user] =+ 1
+            print(user)
+
+
+def error_processing(file_contents):
+    """
+        Input: all lines from the log file.
+        Outputs error dict of most common error messages with most common first.
+    """
+    error = {}
+    error_pattern = r'ERROR ([\w ]*) \('
+    for line in file_contents:
+        if re.search(error_pattern, line) != None:
+            error_text = re.find(error_pattern, line.strip())
+            print(error_text)
 
 def user_csv_generator(per_user):
     pass
@@ -41,10 +54,11 @@ def error_csv_generator(error):
 if __name__ == '__main__':
     try:
         if sys.argv[1] != None:
-            create_dictionaries()
-            #user_dict, error_dict = create_dictionaries()
-            #user_csv_generator(user_dict)
-            #error_csv_generator(error_dict)
+            file_contents = get_data()
+            #per_user = info_processing(file_contents)
+            #error = error_processing(file_contents)
+            #user_csv_generator(per_user)
+            #error_csv_generator(error)
         else:
             print('Pass in an arguement for the log file')
     except:
